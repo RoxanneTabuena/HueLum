4c44c7c1d4432b9d30828b58e1f1ec041a0ddd40
Roxanne Tabuena <roxyannet@gmail.com>
2025-07-02 09:03:00 -0700

    ad background color array

--- src/components/landing/landing.content.js
+++ src/components/landing/landing.content.js
@@ -1,1 +1,14 @@
-export const themes = ['orange', 'purple', 'pink', 'blue', 'aqua']
+export const themes = [
+    'orange', 
+    'blueviolet', 
+    'pink', 
+    'slateblue', 
+    'aqua']
+
+export const backgrounds = [
+    'rgba(255, 160, 0, .5)',
+    'rgba(144, 37, 230, .5)', 
+    'rgba(255, 191, 207, .5)', 
+    'rgba(102, 92, 197, .5)',
+    'rgba(47, 252, 252, .5)'
+]
