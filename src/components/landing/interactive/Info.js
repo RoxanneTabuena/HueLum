b5bf80c9d8ddc2f684a73612a9e94fbe63aa7984
Roxanne Tabuena <roxyannet@gmail.com>
2025-07-02 08:41:24 -0700

    display purchase info after 5 seconds

---
+++ src/components/landing/interactive/Info.js
@@ -0,0 +1,11 @@
+
+import style from './interactive.module.css'
+export const Info= () => {
+
+    return (
+        
+        <div className={style.info}>
+            info
+        </div>
+    )
+}
--- src/components/landing/interactive/Interactive.js
+++ src/components/landing/interactive/Interactive.js
@@ -1,9 +1,21 @@
 import { Cover } from '../cover/Cover'
+import { Info } from './Info'
+import { useState, useEffect } from 'react'
 import style from './interactive.module.css'
 export const Interactive= () => {
+    const [start, setStart] = useState(false)
+    useEffect(()=>{
+        const initiate = setTimeout(()=>{
+            setStart(true)
+        }, 5000)
+        return ()=>clearTimeout(initiate)
+    },[start])
     return (
+        
         <div className={style.interactive}>
-            <Cover/>
+            {start? 
+                <Info/> : <Cover/>    
+            }
         </div>
     )
 }
